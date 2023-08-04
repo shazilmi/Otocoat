707c8f7c1552a9edd83366e746f8fca4fce429ba
shazilmi <shazilmiqbal@protonmail.com>
2023-08-04 08:59:47 +0530

    Final error fix

--- routes/about.py
+++ routes/about.py
@@ -5,9 +5,13 @@ abouts = Blueprint('about', __name__)
 @abouts.route('/about', methods = ["POST", "GET"])
 def about():
 	if request.method == "GET":
-		if session['admin'] == 0:
-			return render_template('fabout.html')
-		elif session['admin'] == 1:
-			return render_template('aabout.html')
-		else:
+		try:
+			if session['admin'] == 0:
+				return render_template('fabout.html')
+			elif session['admin'] == 1:
+				return render_template('aabout.html')
+			else:
+				return render_template('about.html')
+		except:
+			session['admin'] = 2
 			return render_template('about.html')
--- routes/contact.py
+++ routes/contact.py
@@ -5,9 +5,13 @@ contacts = Blueprint('contact', __name__)
 @contacts.route('/contact', methods = ["POST", "GET"])
 def about():
 	if request.method == "GET":
-		if session['admin'] == 0:
-			return render_template('fcontact.html')
-		elif session['admin'] == 1:
-			return render_template('acontact.html')
-		else:
+		try:
+			if session['admin'] == 0:
+				return render_template('fcontact.html')
+			elif session['admin'] == 1:
+				return render_template('acontact.html')
+			else:
+				return render_template('contact.html')
+		except:
+			session['admin'] = 2
 			return render_template('contact.html')
